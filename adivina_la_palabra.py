e198eab16affd0762d20c4382566cd9352f537fb
candelagomez <candelagomez120@gmail.com>
2026-02-01 23:45:40 -0300

    Segundo commit

--- adivina_la_palabra.py
+++ adivina_la_palabra.py
@@ -20,3 +20,26 @@ tablero = []
 
 for letra in palabra_secreta:
     tablero.append("_")
+
+while vidas > 0:
+    print(f"Vidas: {vidas}")
+    print(tablero)
+    letra_ingresada = input("Ingresa una letra: ").lower()
+    if letra_ingresada.isalpha():
+
+        if letra_ingresada in palabra_secreta:
+            print("¡Adivinaste una letra!")
+
+            for i in range(len(palabra_secreta)):
+                if palabra_secreta[i] == letra_ingresada:
+                    tablero[i] = letra_ingresada
+            if "_" not in tablero:
+                print(f"Ganaste, la palabra era: {palabra_secreta}!")
+                print(tablero) 
+                vidas = 0 
+        else:
+            print("Incorrecto, esa letra no está")
+            vidas = vidas - 1
+
+    else:
+        print("Error: Por favor ingresa solo letras.")
